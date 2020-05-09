b214136960d64a515bca62c7cb342577d7c02f4c
tarik nachat <tariknachat@gmail.com>
2020-05-09 04:44:10 +0000

    deleting branch files

--- src/components/sections/success.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class=" bg-green-100 py-2 px-4  rounded border border-green-400 shadow-md">
-        <h5 class="font-mont text-xl font-bold block text-green-600">Confirme Your Email !</h5>
-        <p class="font-lato text-base text-green-700">Your item has been sent to your inbox</p>
-    </div>
-</template>
